5af58539b2d1819ba5dd2e4d06de2bf83f354098
blurryface2809 <grazimashado@gmail.com>
2022-09-13 15:59:07 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt("digite um número de 1 a 10 para saber sua tabuada"))
+const indice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for (let i in indice){
+    console.log(indice[i]*numero)
+}
+
